7e7beefa24db1b587897f2bb7d70e2d71bce3971
shashi <shashiweb64@gmail.com>
2025-06-29 13:28:33 +0530

    create form

---
+++ src/Myform.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Myform() {
+  return (
+    <div className='container bg-danger'>
+        <div className='row'>
+            <div className='col'>
+                <h1>this is my form page</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Myform
--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import Abc from './About';
 import Contact from './Contact';
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
-
 import "./style.css";
+import Myform from './Myform';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Myform></Myform>
     <Abc></Abc>
     <Contact></Contact>
 
