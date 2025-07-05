f0cee98f02b02b0f489144854cbab7c0b2218ceb
shashi <shashiweb64@gmail.com>
2025-07-05 12:17:16 +0530

    change

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Abc from './About';
-import Contact from './Contact';
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
-import "./style.css";
-import Myform from './Myform';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Myform></Myform>
-    <Abc></Abc>
-    <Contact></Contact>
+ 
 
   </React.StrictMode>
 );
